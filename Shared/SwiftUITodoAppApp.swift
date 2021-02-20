53848d5c8a169eab5c5c6c9ec89ef6176049372f
Chung Wei Leong <chungwei@whiteroom.work>
2021-02-20 20:44:16 +0800

    Initial Commit

---
+++ Shared/SwiftUITodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITodoAppApp.swift
+//  Shared
+//
+//  Created by White Room 02 on 20/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
